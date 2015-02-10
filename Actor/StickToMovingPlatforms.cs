dc8dfecb65eabad775cd9d9c2737f6ce1de070e1
Cary Miller <cary.a.miller@gmail.com>
2015-02-10 12:15:26 -0800

    Update StickToMovingPlatforms—still rather inelegant.

--- Actor/StickToMovingPlatforms.cs
+++ Actor/StickToMovingPlatforms.cs
@@ -6,15 +6,23 @@ using System.Collections;
 
 public class StickToMovingPlatforms : CacheBehaviour
 {
+    private bool isPlayer;
+
     void Start()
     {
         base.CacheComponents();
+        isPlayer = (gameObject.name == "Player");
     }
 
     void OnTriggerEnter2D (Collider2D coll)
     {
         if (coll.gameObject.tag == "MovingPlatform")
+        {
             transform.parent = coll.gameObject.transform;
+            
+            if (isPlayer && coll.gameObject.GetComponent<MovingPlatform>().fastPlatform)
+                Messenger.Broadcast<bool>("riding fast platform", true);
+        }
     }
 
     void OnTriggerStay2D (Collider2D coll)
@@ -26,6 +34,11 @@ public class StickToMovingPlatforms : CacheBehaviour
     void OnTriggerExit2D (Collider2D coll)
     {
         if (coll.gameObject.tag == "MovingPlatform")
+        {
             transform.parent = null;
+
+            if (isPlayer)
+                Messenger.Broadcast<bool>("riding fast platform", false);
+        }
     }
 }
--- Game/GameData.cs
+++ Game/GameData.cs
@@ -7,18 +7,28 @@ public class GameData : MonoBehaviour
 	// player stats.
     private static int _currentScore;     	    
     private static int _lastSavedScore;
-    private static int _lives;
+    private static int _lives = 3;
 
     // level stats.
     private static int _currentLevel;  
+
+	public static int GetScore()
+	{
+		return _currentScore;
+	}
 
 	public static void IncreaseScore(int addScore)
 	{
 		_currentScore += addScore;
 	}
 
-	public static int GetScore()
+	public static int GetLives()
 	{
-		return _currentScore;
+		return _lives;
+	}
+
+	public static void IncreaseLives(int addLives)
+	{
+		_lives += addLives;
 	}
 }
