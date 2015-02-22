8593327bc454c350070e6a9a4148a0fcd77a4d12
Cary Miller <cary.a.miller@gmail.com>
2015-02-21 23:26:10 -0800

    Add new fade-in for HUD text.

--- HUD/DisplayScoreFX.cs
+++ HUD/DisplayScoreFX.cs
@@ -8,22 +8,27 @@ public class DisplayScoreFX : BaseBehaviour
 {
 	private Text scoreUI;
 	private int currentscore;
+	private float timeToFade = 2f;
 
-	void Start()
+	void Start ()
 	{
 		scoreUI = gameObject.GetComponent<Text>();
 		scoreUI.text = currentscore.ToString();
+		MTween.FadeOutText(scoreUI, 0, 0);
+		MTween.FadeInText(scoreUI, 1, timeToFade);
 	}
 
 	// EVENT LISTENERS
 	void OnEnable()
 	{
 		Messenger.AddListener<int>("change score", OnChangeScore);
+		Messenger.AddListener<string, Collider2D>( "player dead", OnPlayerDead);
 	}
 	
 	void OnDestroy()
 	{
 		Messenger.RemoveListener<int>("change score", OnChangeScore);
+		Messenger.RemoveListener<string, Collider2D>( "player dead", OnPlayerDead);
 	}
 
 	// EVENT RESPONDERS
@@ -31,5 +36,10 @@ public class DisplayScoreFX : BaseBehaviour
 	{
 		scoreUI.text = newScore.ToString();
 		MTween.DisplayScoreFX(gameObject, scoreUI);
+	}
+
+	void OnPlayerDead(string methodOfDeath, Collider2D coll)
+	{
+		MTween.FadeOutText(scoreUI, .5f, timeToFade);
 	}
 }
