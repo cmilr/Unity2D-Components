5f51d63ab175ed40001eace0db4144f39b6c52d1
Cary Miller <cary.a.miller@gmail.com>
2015-03-05 09:03:22 -0800

    More Unity 5 bug fixes.

--- Scene/SunlightManager.cs
+++ Scene/SunlightManager.cs
@@ -5,40 +5,66 @@ using Matcha.Game.Tweens;
 
 public class SunlightManager : CacheBehaviour {
 
-	private GameObject player;
+	private float fadeAfter = 0f;
+	private float timeToFade = 1f;
 	private PlayerState playerState;
-	private float groundLine;				// coordinates on TileMap where above ground and below ground meet
-	private float aboveGround = 1.45f;		// light intensity when player is above ground
-	private float belowGround = .9f;		// light intensity when player is below ground
+	private Light playerLight;
+	private Light creatureLight;
+	private Light pickupLight;
+	private Light tileLight;
+	private Light planeLight;
+	private float groundLine;						// coordinates on TileMap where above ground and below ground meet
+	private float aboveGround = 1.44f;				// light intensity when player is above ground
+	private float playerBelowGround = .82f;			// light intensity when player is below ground
+	private float creatureBelowGround = .82f;		// light intensity when player is below ground
+	private float pickupBelowGround = .82f;			// light intensity when player is below ground
+	private float tileBelowGround = .64f;			// light intensity when player is below ground
+	private float planeAboveBelowGround = .13f;		// light intensity when player is below ground
 
 	void Start () 
 	{
-		player = GameObject.Find("Player");
-		playerState = player.GetComponent<PlayerState>();
+		playerState = GameObject.Find("Player").GetComponent<PlayerState>();
+		playerLight = GameObject.Find("PlayerLight").GetComponent<Light>();
+		creatureLight = GameObject.Find("CreatureLight").GetComponent<Light>();
+		pickupLight = GameObject.Find("PickupLight").GetComponent<Light>();
+		tileLight = GameObject.Find("TileLight").GetComponent<Light>();
+		planeLight = GameObject.Find("PlaneLight").GetComponent<Light>();
 
-		// light.intensity = aboveGround;
+		OnAboveGround();
 
 		InvokeRepeating("CheckIfAboveGround", 0f, 0.3F);
 	}
 
 	void CheckIfAboveGround()
 	{
-		// float targetIntensity;
-		// float fadeAfter = 0f;
-		// float timeToFade = 1f;
+		if (playerState.GetY() > groundLine)
+		{
+			OnAboveGround();
+			Messenger.Broadcast<bool>("player above ground", true);
+		}
+		else 
+		{
+			OnBelowGround();
+			Messenger.Broadcast<bool>("player above ground", false);
+		}
+	}
 
-		// if (playerState.GetY() > groundLine)
-		// {
-		// 	targetIntensity = aboveGround;
-		// 	Messenger.Broadcast<bool>("player above ground", true);
-		// }
-		// else 
-		// {
-		// 	targetIntensity = belowGround;
-		// 	Messenger.Broadcast<bool>("player above ground", false);
-		// }
+	void OnAboveGround()
+	{
+		playerLight.intensity = aboveGround;
+		creatureLight.intensity = aboveGround;
+		pickupLight.intensity = aboveGround;
+		tileLight.intensity = aboveGround;
+		planeLight.intensity = planeAboveBelowGround;
+	}
 
-		// MTween.FadeIntensity(light, targetIntensity, fadeAfter, timeToFade);
+	void OnBelowGround()
+	{
+		MTween.FadeIntensity(playerLight, playerBelowGround, fadeAfter, timeToFade);
+		MTween.FadeIntensity(creatureLight, creatureBelowGround, fadeAfter, timeToFade);
+		MTween.FadeIntensity(pickupLight, pickupBelowGround, fadeAfter, timeToFade);
+		MTween.FadeIntensity(tileLight, tileBelowGround, fadeAfter, timeToFade);
+		MTween.FadeIntensity(planeLight, planeAboveBelowGround, fadeAfter, timeToFade);
 	}
 	
 	void OnSetGroundLine(float coordinates)
