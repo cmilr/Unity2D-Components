58a4f6157e3721c43123e419b1e38b516065de08
Cary Miller <cary.a.miller@gmail.com>
2015-03-05 21:59:07 -0800

    Refactor LightManager.

--- Scene/LightManager.cs
+++ Scene/LightManager.cs
@@ -13,6 +13,7 @@ public class LightManager : CacheBehaviour {
 	private Light pickupLight;
 	private Light tileLight;
 	private Light planeLight;
+<<<<<<< HEAD:Assets/Scripts/Scene/LightManager.cs
 	// coordinates on TileMap where ground begins
 	private float groundLine;									
 	// above ground light intensity
@@ -27,6 +28,15 @@ public class LightManager : CacheBehaviour {
 	private float pickupBelowGround = 1.1f;			
 	private float tileBelowGround = .64f;			
 	private float planeBelowGround = .13f;			
+=======
+	private float groundLine;						// coordinates on TileMap where above ground and below ground meet
+	private float aboveGround = 1.95f;				// light intensity when player is above ground
+	private float playerBelowGround = 1.17f;		// light intensity when player is below ground
+	private float creatureBelowGround = 1.17f;		// light intensity when player is below ground
+	private float pickupBelowGround = 1.1f;			// light intensity when player is below ground
+	private float tileBelowGround = .64f;			// light intensity when player is below ground
+	private float planeAboveBelowGround = .13f;		// light intensity when player is below ground
+>>>>>>> c241ff51d911cd89ed8dc6a108d25085e4e7771a:Assets/Scripts/Scene/SunlightManager.cs
 
 	void Start () 
 	{
