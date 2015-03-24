6999c6ed862ae94e94c531a1e4db43550bdc7642
Cary Miller <cary.a.miller@gmail.com>
2015-03-24 15:21:10 -0700

    Minor Tweaks.

--- Behaviours/BaseBehaviour.cs
+++ Behaviours/BaseBehaviour.cs
@@ -11,19 +11,25 @@ using System.Collections;
 
 public class BaseBehaviour : MonoBehaviour {
 
+    // gameObject names
     protected const string PLAYER            = "Player";
     protected const string _GAME_DATA        = "_GameData";
     protected const string GAME_STATE        = "GameState";
     protected const string TILE_MAP          = "TileMap";
     protected const string DEFAULT_TILE      = "DefaultTile";
 
+    // player/weapon animation speeds
     protected const float IDLE_SPEED         = 1f;
     protected const float RUN_SPEED          = .5f;
     protected const float JUMP_SPEED         = 5f;
 
+    // entity auto-alignment settings
     protected const float ALIGN_ENTITY_TO    = .124f;
+
+    // hud fading speeds on level loading
     protected const float HUD_FADE_IN_AFTER  = .75f;
     protected const float HUD_FADE_OUT_AFTER = .25f;
+
 
     protected void Dbug()
     {
