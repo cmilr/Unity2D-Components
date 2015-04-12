aeaf7f237c686a9fd3ecf0d9e6f7a12cd1efa7a3
jguarShark <jguarshark@gmail.com>
2015-04-11 22:04:16 -0700

    Refactor AnimationOffset routine, for fewer calls. Solves issue #3.

--- Behaviours/AnimationBehaviour.cs
+++ Behaviours/AnimationBehaviour.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class AnimationBehaviour : CacheBehaviour {
 
+    protected float previousX = 0f;
+    protected float previousY = 0f;
+
     protected void OffsetAnimation(float xOffset, float yOffset)
     {
-        transform.localPosition = new Vector3(xOffset, yOffset, 0);
+        if (xOffset != previousX || yOffset != previousY)
+            transform.localPosition = new Vector3(xOffset, yOffset, 0);
+
+        previousX = xOffset;
+        previousY = yOffset;
     }
 }
