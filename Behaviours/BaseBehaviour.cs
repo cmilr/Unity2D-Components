0a5808d7d94b08b1441bb9c017d4052712d86107
jguarShark <cary.a.miller@gmail.com>
2015-04-16 13:05:49 -0700

    Fix HUD layout.

--- Behaviours/BaseBehaviour.cs
+++ Behaviours/BaseBehaviour.cs
@@ -27,7 +27,7 @@ public class BaseBehaviour : MonoBehaviour {
     protected const string TILE_MAP             = "TileMap";
     protected const string DEFAULT_TILE         = "DefaultTile";
 
-    // layer names
+    // layer names for colliders
     protected const int PLAYER_COLLIDER         = 12;
     protected const int WEAPON_COLLIDER         = 9;
     protected const int BODY_COLLIDER           = 10;
