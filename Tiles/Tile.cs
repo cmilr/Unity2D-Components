c2051b3c175f30acf97e356cb38c7c5e0cebcba6
jguarShark <cary.a.miller@gmail.com>
2015-07-15 10:50:18 -0700

    Begin refactoring of general spaghetti code. Simplify tile prefabs.

--- Tiles/Tile.cs
+++ Tiles/Tile.cs
@@ -3,13 +3,16 @@
 
 public class Tile : BaseBehaviour
 {
+    // TileType is used to block enemy AI from walking off into space, etc
+    public enum TileType { Decorative, Solid, OneWay };
+    public TileType tileType = TileType.Decorative;
     private Shader shader1;
     private MeshRenderer meshRenderer;
 
-
     void Start() {
-        meshRenderer                   = transform.parent.GetComponent<MeshRenderer>();
+
         shader1                        = Shader.Find("Sprites/Diffuse");
+        meshRenderer                   = transform.parent.GetComponent<MeshRenderer>();
         meshRenderer.material.shader   = shader1;
         meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
         meshRenderer.receiveShadows    = false;
