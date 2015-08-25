265c06f0af8903501ef4edc18a3335a69807857e
jguarShark <cary.a.miller@gmail.com>
2015-08-25 14:20:52 -0700

    Experiment with breakable enemies.

---
+++ Enemies/Breakable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Breakable : CacheBehaviour {
+
+    private float xRectPosition;
+    private float yRectPosition;
+
+	// Use this for initialization
+	void Start () {
+
+        xRectPosition = spriteRenderer.sprite.rect.x;
+        Debug.Log(xRectPosition);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
