ea7b43d5581c261c51999fb92e8aeeaa1553993e
Cary Miller <cary.a.miller@gmail.com>
2017-07-28 18:18:48 -0700

    Add classes for debugging in the editor's 'Play' mode.
    - InEditorAutoUpdater.cs keeps camera settings constantly updated
    during play mode in the editor.
    
    - QuickDebugger.cs allows for turning various settings on/off.
    while in play mode, such as vSync, enemy attack, and player movement.

---
+++ Debug/InEditorAutoUpdater.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// keeps editor Game View in sync with camera & resolution settings.
+// press play to refresh Game View.
+
+[ExecuteInEditMode]
+public class InEditorAutoUpdater : BaseBehaviour
+{
+#if UNITY_EDITOR
+
+	private CanvasScaler[] canvasScaler;
+	private float orthoSize;
+	private float canvasScale;
+	private int pixelsPerUnit;
+
+	void Start()
+	{
+		canvasScaler = Camera.main.GetComponentsInChildren<CanvasScaler>();
+		canvasScale = Camera.main.GetComponent<PixelArtCamera>().CanvasScale;
+	}
+
+	void Update()
+	{
+		SetCameraOrthographicSize();
+	}
+
+	void SetCameraOrthographicSize()
+	{
+		foreach (CanvasScaler c in canvasScaler)
+		{
+			c.scaleFactor = canvasScale;
+		}
+	}
+
+	void RefreshGameView()
+	{
+		UnityEditorInternal.InternalEditorUtility.RepaintAllViews();
+	}
+
+	void OnApplicationQuit()
+	{
+		SetCameraOrthographicSize();
+	}
+
+#endif
+}
