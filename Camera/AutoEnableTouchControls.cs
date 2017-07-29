27f48dca99fe43ef513fc82e52300c125160aa09
Cary Miller <cary.a.miller@gmail.com>
2017-07-28 18:12:06 -0700

    Add class to auto-activate touch controls.

---
+++ Camera/AutoEnableTouchControls.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+[ExecuteInEditMode]
+public class AutoEnableTouchControls : BaseBehaviour
+{
+	private GameObject canvasGo;
+	private GameObject eventSystem;
+
+	void Start()
+	{
+		canvasGo = gameObject.transform.Find("Canvas").gameObject;
+		Assert.IsNotNull(canvasGo);
+
+		eventSystem = gameObject.transform.Find("EventSystem").gameObject;
+		Assert.IsNotNull(eventSystem);
+
+		#if UNITY_STANDALONE
+			canvasGo.SetActive(false);
+			eventSystem.SetActive(false);
+		#endif
+
+		#if UNITY_IOS
+			canvasGo.SetActive(true);
+			eventSystem.SetActive(true);
+		#endif
+	}
+}
