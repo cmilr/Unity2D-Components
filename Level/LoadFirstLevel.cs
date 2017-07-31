48af2d84e40de1cbf730babdc5236d191679a1d6
Cary Miller <cary.a.miller@gmail.com>
2017-07-31 12:10:45 -0700

    Allow unity to preload assets for first level

---
+++ Level/LoadFirstLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+// allows Unity to pre-load assets for first actual level.
+public class LoadFirstLevel : BaseBehaviour {
+
+	void Start() 
+	{
+		Debug.Log("LoadFirstLevel called");
+		SceneManager.LoadScene("Level1");
+	}
+}
