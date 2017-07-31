9661538787aa57a6322a970fc3ee9cadb993fbb6
Cary Miller <cary.a.miller@gmail.com>
2017-07-31 10:45:37 -0700

    Add interface for main character animations

---
+++ Interfaces/IPlayerAnimator.cs
@@ -0,0 +1,11 @@
+﻿
+public interface IPlayerAnimator
+{
+	void PlayIdleAnimation();
+	void PlayRunAnimation();
+	void PlayJumpAnimation();
+	void PlayAttackAnimation();
+	void PlayRunAttackAnimation();
+	void PlayJumpAttackAnimation();
+	bool ExitTimeReached();
+}
